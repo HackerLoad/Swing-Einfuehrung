17eca0b5a50fef385d4393ef32dd4483eab7d5a5
Maxim <hackerloade@gmail.com>
2025-07-08 14:48:28 +0200

    Update Start.java

--- Start.java
+++ Start.java
@@ -3,6 +3,12 @@ import javax.swing.*;
 public class Start {
 
     public static void main(String[] args) {
+        JMenuBar meinMenue = new JMenuBar();                    //ERSTELLT EINE MENÜLEISTE
+        JMenu datei = new JMenu("Datei");                    //ERSTELLT MENÜEINTRAG "DATEI"
+        JMenuItem close = new JMenuItem("Beenden");        //ERSTELLT EINTRAG "BEENDEN" ALS DROPDOWN
+
+        datei.add(close);
+        meinMenue.add(datei);
         JFrame meins = new JFrame("Mein erstes GUI");       //ERSTELLT GUI
         meins.setSize(1024, 768);                   //STELLT GRÖßE EIN
         meins.setResizable(false);                              //STELLT EIN OB BENUTZER FENSTER VERÄNDERN KANN
@@ -10,7 +16,10 @@ public class Start {
 
         meins.add(new JLabel("Hello World!"));              //FÜGT TEXT HINZU
 
+        meins.setJMenuBar(meinMenue);
         meins.setVisible(true);                                 //SICHTBARKEIT
+
+        meins.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //SCHLIEßT DAS FENSTER WENN MAN AUF X DRÜCKT
 
     }
 
