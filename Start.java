7599a1c85978e4b15167c3897f7b0f81a7fbffe4
Maxim <hackerloade@gmail.com>
2025-07-08 14:35:58 +0200

    Update Start.java

--- Start.java
+++ Start.java
@@ -5,6 +5,8 @@ public class Start {
     public static void main(String[] args) {
         JFrame meins = new JFrame("Mein erstes GUI");       //ERSTELLT GUI
         meins.setSize(1024, 768);                   //STELLT GRÖßE EIN
+        meins.setResizable(false);                              //STELLT EIN OB BENUTZER FENSTER VERÄNDERN KANN
+        meins.setLocationRelativeTo(null);                      //ZENTRIERT DAS FENSTER AUF DEM BILDSCHIRM
 
         meins.add(new JLabel("Hello World!"));              //FÜGT TEXT HINZU
 
